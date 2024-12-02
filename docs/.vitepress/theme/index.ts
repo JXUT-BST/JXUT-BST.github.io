ce0d529bb6711e6090a83a3d977337ac2e9a8f10
Alaye-Dong <AlayeVast@outlook.com>
2024-12-02 16:58:04 +0800

    feat: ✨ 通过giscus实现评论功能

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -7,6 +7,9 @@ import './style.css'
 import '@theojs/lumen/doc-blocks'
 import { HomeUnderline } from '@theojs/lumen'
 
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { useData, useRoute } from 'vitepress';
+
 import Footer from "./components/Footer.vue"
 
 export default {
@@ -20,5 +23,29 @@ export default {
     // 注册全局组件
     app.component('Footer', Footer)
     app.component('Home', HomeUnderline) 
+  },
+  setup() {
+    // Get frontmatter and route
+    const { frontmatter } = useData();
+    const route = useRoute();
+
+    // giscus配置
+    giscusTalk({
+      repo: 'JXUT-BST/JXUT-BST.github.io', //仓库
+      repoId: 'R_kgDOM_5JJQ', //仓库ID
+      category: 'Announcements', // 讨论分类
+      categoryId: 'DIC_kwDOM_5JJc4CkzXy', //讨论分类ID
+      mapping: 'pathname',
+      inputPosition: 'bottom',
+      lang: 'zh-CN',
+    },
+      {
+        frontmatter, route
+      },
+      //默认值为true，表示已启用，此参数可以忽略；
+      //如果为false，则表示未启用
+      //您可以使用“comment:true”序言在页面上单独启用它
+      true
+    );
   }
 } satisfies Theme
