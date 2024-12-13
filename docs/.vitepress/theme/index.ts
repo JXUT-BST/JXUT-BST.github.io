3bb4fc9de8aecd45ebfc2670a0263edee8c14f64
Alaye-Dong <AlayeVast@outlook.com>
2024-12-14 02:59:33 +0800

    feat: ✨ 添加阅读增强插件

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -14,6 +14,12 @@ import {
 } from '@nolebase/vitepress-plugin-thumbnail-hash/client'
 import '@nolebase/vitepress-plugin-thumbnail-hash/client/style.css'
 
+import {
+  NolebaseEnhancedReadabilitiesMenu,
+  NolebaseEnhancedReadabilitiesScreenMenu,
+} from '@nolebase/vitepress-plugin-enhanced-readabilities/client'
+import '@nolebase/vitepress-plugin-enhanced-readabilities/client/style.css'
+
 // import '@theojs/lumen/doc-blocks'
 import '@theojs/lumen/icon' /* 图标 */
 import { HomeUnderline } from '@theojs/lumen'
@@ -28,6 +34,10 @@ export default {
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      // 为较宽的屏幕的导航栏添加阅读增强菜单
+      'nav-bar-content-after': () => h(NolebaseEnhancedReadabilitiesMenu),
+      // 为较窄的屏幕（通常是小于 iPad Mini）添加阅读增强菜单
+      'nav-screen-content-after': () => h(NolebaseEnhancedReadabilitiesScreenMenu), 
     })
   },
   enhanceApp({ app, router, siteData }) {
