4cd899194d2a93c46111304cf5f661ffbf1a5f15
Alaye-Dong <AlayeVast@outlook.com>
2025-09-16 15:05:23 +0800

    refactor(posts): 将 blog.data 重命名为 posts.data 并优化相关组件

--- .vitepress/theme/components/BlogIndex.vue
+++ .vitepress/theme/components/BlogIndex.vue
@@ -1,6 +1,6 @@
 <!-- 实现方式参考Vite官网Blog页面 https://github.com/vitejs/vite/blob/main/docs/.vitepress/theme/components/BlogIndex.vue -->
 <script setup lang="ts">
-import { data as posts } from "@docs/_data/blog.data";
+import { data as posts } from "@docs/_data/posts.data";
 
 function getDateTime(time: number) {
 	return new Date(time).toISOString();
@@ -8,8 +8,8 @@ function getDateTime(time: number) {
 </script>
 
 <template>
-  <ul class="blog-list">
-    <li class="blog-entry" v-for="post of posts">
+  <ul class="post-list">
+    <li class="post-entry" v-for="post of posts">
       <a :href="post.url">
         <article>
           <time :datetime="getDateTime(post.date.time)">{{
@@ -25,28 +25,28 @@ function getDateTime(time: number) {
 </template>
 
 <style scoped>
-.blog-list {
+.post-list {
   list-style-type: none;
   padding: 0;
 }
-.blog-entry {
+.post-entry {
   margin-top: 1em;
   border-bottom: 1px solid var(--vp-c-divider);
 }
 
-.blog-entry article {
+.post-entry article {
   padding: 1.5em;
   border-radius: 8px; /* 圆角 */
   box-shadow: var(--vp-shadow-1); /* 阴影效果 */
   transition: transform 0.2s ease, box-shadow 0.2s ease; /* 添加过渡效果 */
 }
 
-.blog-entry article:hover {
+.post-entry article:hover {
   transform: translateY(-5px); /* 鼠标悬停时轻微上移 */
   box-shadow: var(--vp-shadow-2); /* 鼠标悬停时增强阴影 */
 }
 
-.blog-entry time {
+.post-entry time {
   font-size: 14px;
   color: var(--vp-c-text-1);
 }
@@ -58,7 +58,7 @@ function getDateTime(time: number) {
   font-size: 22px;
 }
 
-.blog-entry a {
+.post-entry a {
   text-decoration: none;
   transition: color 0.2s ease; /* 添加颜色过渡 */
 }
